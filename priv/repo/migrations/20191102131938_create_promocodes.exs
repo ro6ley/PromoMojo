7d683c4e7fee5c05153f08745dba0013627bd5e7
Robley <robleyadrian@gmail.com>
2019-11-02 17:14:48 +0300

    add priv folder

---
+++ priv/repo/migrations/20191102131938_create_promocodes.exs
@@ -0,0 +1,21 @@
+defmodule PromoMojo.Repo.Migrations.CreatePromocodes do
+  use Ecto.Migration
+
+  def change do
+    create table(:promocodes, primary_key: false) do
+      add :id, :binary_id, primary_key: true
+      add :code, :string
+      add :value, :integer
+      add :radius, :float
+      add :radius_unit, :string
+      add :is_active, :boolean, default: false, null: false
+      add :expiry, :naive_datetime
+      add :location_latitude, :float
+      add :location_longitude, :float
+
+      timestamps()
+    end
+
+    create unique_index(:promocodes, [:code])
+  end
+end
