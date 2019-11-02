0bfb48483f7b0dd1a32cbb09330fda5546b71590
Robley <robleyadrian@gmail.com>
2019-11-02 17:12:24 +0300

    add project config folder

---
+++ config/test.exs
@@ -0,0 +1,18 @@
+use Mix.Config
+
+# Configure your database
+config :promo_mojo, PromoMojo.Repo,
+  username: "postgres",
+  password: "postgres",
+  database: "promo_mojo_test",
+  hostname: "localhost",
+  pool: Ecto.Adapters.SQL.Sandbox
+
+# We don't run a server during test. If one is required,
+# you can enable the server option below.
+config :promo_mojo, PromoMojoWeb.Endpoint,
+  http: [port: 4002],
+  server: false
+
+# Print only warnings and errors during test
+config :logger, level: :warn
