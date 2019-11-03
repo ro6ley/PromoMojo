1ebc38418ec8cacfc3dcad5bbb77cb2b9e4d7993
Robley <robleyadrian@gmail.com>
2019-11-03 18:19:55 +0300

    add script to start the API

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# update this
+export DATABASE_URL=ecto://USERNAME:PASSWORD@HOST:PORT/DATABASE_NAME
+export SECRET_KEY_BASE=verylongandsecret
+# update this
+export GOOGLE_API_KEY=GOOGLE-API-KEY-WITH-DIRECTIONS-API-ENABLED
+
+if [[ $1 == "prod" || $1 == "--prod" ]]; then
+    echo "Running PromoMojo in production mode"
+    MIX_ENV=prod
+else
+    echo "Running in dev mode"
+fi
+
+PORT=4000
+mix phx.server
