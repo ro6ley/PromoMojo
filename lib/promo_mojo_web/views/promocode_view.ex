68c562db6533c97be16118d10b54f25147ce64f8
Robley <robleyadrian@gmail.com>
2019-11-03 15:17:45 +0300

    add response code to view

--- lib/promo_mojo_web/views/promocode_view.ex
+++ lib/promo_mojo_web/views/promocode_view.ex
@@ -10,6 +10,10 @@ defmodule PromoMojoWeb.PromocodeView do
     %{data: render_one(promocode, PromocodeView, "promocode.json")}
   end
 
+  def render("redeem.json", %{res_data: res_data, code: code}) do
+    %{data: res_data, code: code}
+  end
+
   def render("promocode.json", %{promocode: promocode}) do
     %{id: promocode.id,
       code: promocode.code,
@@ -18,7 +22,6 @@ defmodule PromoMojoWeb.PromocodeView do
       radius_unit: promocode.radius_unit,
       is_active: promocode.is_active,
       expiry: promocode.expiry,
-      location_latitude: promocode.location_latitude,
-      location_longitude: promocode.location_longitude}
+      location: promocode.location}
   end
 end
