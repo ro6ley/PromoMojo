bb60b1747e19134393c5016210fad1a2262db6e6
Robley <robleyadrian@gmail.com>
2019-11-03 17:04:36 +0300

    refactor error view test

--- test/promo_mojo_web/views/error_view_test.exs
+++ test/promo_mojo_web/views/error_view_test.exs
@@ -5,11 +5,11 @@ defmodule PromoMojoWeb.ErrorViewTest do
   import Phoenix.View
 
   test "renders 404.json" do
-    assert render(PromoMojoWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+    assert render(PromoMojoWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Promocode not found."}, code: 404}
   end
 
   test "renders 500.json" do
     assert render(PromoMojoWeb.ErrorView, "500.json", []) ==
-             %{errors: %{detail: "Internal Server Error"}}
+             %{errors: %{detail: "Internal Server Error"}, code: 500}
   end
 end
